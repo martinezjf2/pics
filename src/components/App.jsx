498182cb2cc005d5977a49674f0caa160f58d79f
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-03 16:07:11 -0500

    added code within the searchbar and imported and exported

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import SearchBar from "./SearchBar"
 
 function App() {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <h1><SearchBar /></h1>
         </div>
     )
 }
