fa49b285303a3994f00403744753310fdc80b037
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-03 16:50:49 -0500

    refractored and called initialized state

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = {term: ''}
 
-    onInputChange(event) {
-      console.log( event.target.value )
-    }
+    // onInputChange(event) {
+    //     this.setState({ term: event.target.value })
+    // }
 
     render() {
         return (
@@ -12,7 +13,7 @@ class SearchBar extends React.Component {
             <form className="ui form">
                 <div className="field">
                     <label>Image Search</label>
-                    <input type="text" onChange={this.onInputChange}/>
+                    <input type="text" onChange={(e) => this.setState({term: e.target.value}) } value={this.state.term}/>
                 </div>
             </form>
         </div>
