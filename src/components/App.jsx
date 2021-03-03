693f20ed64bd3e583b4ff807638503c150305e05
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-03 16:14:55 -0500

    added margina on top of the search bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar"
 
 function App() {
     return (
-        <div className="ui container">
+        <div className="ui container" style={{marginTop: '10px'}}>
             <h1><SearchBar /></h1>
         </div>
     )
