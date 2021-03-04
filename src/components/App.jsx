6209f7ce2961c3100c16d0d5f163cedf4941a52e
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-03 22:39:55 -0500

    refractored App comp. to a class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar"
 
-function App() {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <h1><SearchBar /></h1>
-        </div>
-    )
+class App extends React.Component {
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <h1><SearchBar /></h1>
+            </div>
+        )
+    }
 }
 
 export default App;
