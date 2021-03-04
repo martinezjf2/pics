bfdc49bbb1bb707e1eb4152b6be1cb0874433669
Jeffrey Martinez <martinezjf2@gmail.com>
2021-03-03 22:43:59 -0500

    created onSubmit prop from the App comp, and used it in the onFormSubmit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import SearchBar from "./SearchBar"
 
 class App extends React.Component {
+
+    onSearchSubmit = term => {
+        console.log(term);  
+    }
+
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
-                <h1><SearchBar /></h1>
+                <h1><SearchBar onSubmit={this.onSearchSubmit}/></h1>
             </div>
         )
     }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ class SearchBar extends React.Component {
 
     onFormSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.term)
+        this.props.onSubmit(this.state.term)
     }
 
     render() {
